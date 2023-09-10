b92c3a35dea6764af59f0c3d8a29211f1d0f452e
julio cesar <jcmcf2012@gmail.com>
2023-09-10 12:52:59 -0300

    fix: route better protection

--- src/view/ProtectedRoute.jsx
+++ src/view/ProtectedRoute.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-export default function ProtectedRouter(path, element) {
+export default function ProtectedRouter({ userType }) {
   const routerHandler = () => {
     let data = sessionStorage.getItem("user");
     const user = JSON.parse(data);
 
     if (!data) {
       return <Navigate to="/" replace />;
+    }
+    if (user.role !== userType) {
+      return <Navigate to={`/${user.role}`} replace />;
     }
 
     if (user.userId) {
