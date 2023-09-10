d9e28fd0d141c536e56a92befe77dfef24a5442b
julio cesar <jcmcf2012@gmail.com>
2023-09-10 12:53:45 -0300

    feat: add new components

--- src/view/pageMinhasVendas.js
+++ src/view/pageMinhasVendas.js
@@ -1,33 +1,28 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
-import  navHComponent from './navbarhori';
-import  navVendaComponent from './navbarVendedor';
-import minhasVendas from './minhasVendas'
+import navHComponent from "./navbarhori";
+import navVendaComponent from "./navbarVendedor";
+import MinhasVendas from "./MinhasVendas";
 
-export default function pageMinhasVendas(){
-    return (
-      <div className="g-sidenav-show   bg-gray-100">
-        <div id="gradiente" className="min-height-300 bg-primary position-absolute w-100" ></div>
-        {navVendaComponent()}
-        <main class="main-content position-relative border-radius-lg ">
-            {navHComponent()}
-            <div class="container-fluid py-4">
-                <div class="row index-ultimos-anuncios">
-                    {minhasVendas()}
-                </div>
-
-            </div>
-        </main>
-
-
-        
-
-
-      </div>
-    );
-  };
-
-
+export default function pageMinhasVendas() {
+  return (
+    <div className="g-sidenav-show   bg-gray-100">
+      <div
+        id="gradiente"
+        className="min-height-300 bg-primary position-absolute w-100"
+      ></div>
+      {navVendaComponent()}
+      <main class="main-content position-relative border-radius-lg ">
+        {navHComponent()}
+        <div class="container-fluid py-4">
+          <div class="row index-ultimos-anuncios">
+            <MinhasVendas></MinhasVendas>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
